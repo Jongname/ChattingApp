66bc899ba00715f6b9be8145fd885fc8a3e50e9c
Jason <qorrn@gmail.com>
2018-10-10 06:55:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/chattingapp/fragment/PeopleFragment.java
@@ -0,0 +1,99 @@
+package com.example.chattingapp.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.chattingapp.R;
+import com.example.chattingapp.model.UserModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_people,container,false);
+        RecyclerView recyclerView = view.findViewById(R.id.peoplefragment_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        recyclerView.setAdapter(new PeopleFragmentRecyclerViewAdapter());
+
+        return view;
+    }
+    class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+        List<UserModel> userModels;
+        public PeopleFragmentRecyclerViewAdapter() {
+            userModels = new ArrayList<>();
+            FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) { //서버에서 넘어온 데이터
+                    userModels.clear();  //누적되는 데이터 제거되는
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        userModels.add(snapshot.getValue(UserModel.class));
+                    }
+                    notifyDataSetChanged();  //새로고침
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_friend,viewGroup,false);
+
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            //이미지 넣어주는
+
+            Glide.with
+                    (viewHolder.itemView.getContext())
+                    .load(userModels.get(i).profileImageUri)
+                    .apply(new RequestOptions().circleCrop())
+                    .into(((CustomViewHolder)viewHolder).imageView);
+
+            ((CustomViewHolder)viewHolder).textView.setText(userModels.get(i).UserName);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return userModels.size();
+        }
+
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+            public ImageView imageView;
+            public TextView textView;
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = view.findViewById(R.id.frienditem_imageview);
+                textView = view.findViewById(R.id.frienditem_textview);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chattingapp/model/UserModel.java
@@ -0,0 +1,7 @@
+package com.example.chattingapp.model;
+
+public class UserModel
+{
+    public String UserName;
+    public String profileImageUri;
+}
